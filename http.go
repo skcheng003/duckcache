e77c2b9dbdf12bc029fc2effd05c337f20efd57c
Shengkai Cheng <skcheng003@gmail.com>
2023-07-31 17:34:16 +0800

    add HTTPPool struct

--- http.go
+++ http.go
@@ -1,1 +1,10 @@
 package duckcache
+
+const defaultBasePath = "/_duck-cache/"
+
+// HTTPPool implement PeerPicker for a pool of HTTP peers.
+type HTTPPool struct {
+	// this peer's base URL, e.g. "https://example.net:8080"
+	self     string
+	basePath string
+}
