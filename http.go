fb7fb17d4661b06f85c837ddabd3c44c1689c63e
Shengkai Cheng <skcheng003@gmail.com>
2023-08-01 10:50:12 +0800

    add main.go

--- http.go
+++ http.go
@@ -1,5 +1,12 @@
 package duckcache
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
 const defaultBasePath = "/_duck-cache/"
 
 // HTTPPool implement PeerPicker for a pool of HTTP peers.
@@ -15,4 +22,41 @@ func NewHTTPPool(self string) *HTTPPool {
 		self:     self,
 		basePath: defaultBasePath,
 	}
+}
+
+// Log info with server name
+func (p *HTTPPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v))
+}
+
+// ServeHTTP handle all http requests
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+	// /<basepath>/<groupname>/<key> required
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
 }
